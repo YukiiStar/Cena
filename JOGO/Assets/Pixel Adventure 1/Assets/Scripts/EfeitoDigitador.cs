ffe582cd4bcfae89b3ccad048516e5b0daf08716
YukiiStar <ester.loysse@escolar.ifrn.edu.br>
2024-09-09 13:58:03 -0300

    nova cena sobre cutscene

---
+++ JOGO/Assets/Pixel Adventure 1/Assets/Scripts/EfeitoDigitador.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class EfeitoDigitador : MonoBehaviour
+{
+  private TextMeshProUGUI componenteTexto;
+  private AudioSource _audioSource;
+}
