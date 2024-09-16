3834db4d737c79f66bc477f2d520d0227510cbb1
YukiiStar <145374716+YukiiStar@users.noreply.github.com>
2024-09-16 14:35:09 -0300

    .

--- JOGO/Assets/Pixel Adventure 1/Assets/Scripts/EfeitoDigitador.cs
+++ JOGO/Assets/Pixel Adventure 1/Assets/Scripts/EfeitoDigitador.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
@@ -8,4 +9,43 @@ public class EfeitoDigitador : MonoBehaviour
 {
   private TextMeshProUGUI componenteTexto;
   private AudioSource _audioSource;
+  private string mensagemOriginal;
+  public bool imprimindo;
+  public float tempoEntreLetras = 0.08f;
+
+  private void Awake()
+  {
+	  TryGetComponent(out componenteTexto);
+	  TryGetComponent(out _audioSource);
+	  mensagemOriginal = componenteTexto.text;
+	  componenteTexto.text = "";
+  }
+
+  private void OnEnable()
+  {
+
+  }
+
+  private void OnDisable()
+  {
+    componenteTexto.text = mensagemOriginal;
+  }
+
+  public void ImprimirMensagem(string mensagem)
+  {
+
+  }
+
+  IEnumerator LetraPorLetra(string mensagem)
+  {
+	  string msg = "";
+    foreach (var letra in mensagem)
+    {
+      msg += letra;
+      componenteTexto.text = msg;
+      _audioSource.Play();
+      yield return new WaitForSeconds(tempoEntreLetras);
+    }
+    
+  }
 }
